2828f69bdbfffe7f452887ddfecb58fffec06d93
arthurasanaliev <arthur@io.net>
2026-01-30 17:57:23 +0600

    Update vLLM_API_KEY overriding

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -352,11 +352,11 @@ WorkingDirectory=${DEPLOY_DIR}
 # Verify file integrity before starting - fails if files have been tampered with
 ExecStartPre=/bin/bash -c 'cd ${DEPLOY_DIR} && sha256sum -c .integrity-checksums'
 
-ExecStart=${DEPLOY_DIR}/venv/bin/uvicorn main:app --host 0.0.0.0 --port 443 --ssl-keyfile certs/key.pem --ssl-certfile certs/cert.pem
+ExecStart=/bin/bash -c 'export VLLM_API_KEY=\${VLLM_API_KEY:-dummy_key}; ${DEPLOY_DIR}/venv/bin/uvicorn main:app --host 0.0.0.0 --port 443 --ssl-keyfile certs/key.pem --ssl-certfile certs/cert.pem'
 Restart=always
 RestartSec=5
 Environment=PYTHONUNBUFFERED=1
-Environment=VLLM_API_KEY=dummy_key
+EnvironmentFile=-/etc/default/cc-attestation
 
 # Security hardening
 NoNewPrivileges=true
